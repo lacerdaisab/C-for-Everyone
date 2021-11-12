6137ae48eb0c1100ca3bf597ffe1b96fb3139ebe
Isabel Lacerda <contato.lacerdaisab@outlook.com>
2021-11-12 12:37:09 -0300

    Organize with folders

---
+++ Programming Fundamentals/Week 1/Three floats and print sum/Three-floats-and-print-sum.c
@@ -0,0 +1,15 @@
+/* Read in three floats and print sum
+by Isabel Lacerda
+November 12th, 2021 */
+
+#include <stdio.h>
+
+int main () {
+    float a, b, c, sum;
+    printf("Input three numbers:");
+    scanf("%f%f%f", &a, &b, &c);
+    printf("a = %f, b = %f, c= %f\n", a, b, c);
+    sum = a + b + c;
+    printf("sum = %f\n\n", sum);
+    return 0;
+}
