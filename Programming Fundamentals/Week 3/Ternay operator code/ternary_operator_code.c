72edd1db12efdfc7c52ae2af2b0c8ceb389b1cfc
Isabel Lacerda <contato.lacerdaisab@outlook.com>
2021-11-15 07:24:14 -0300

    Learn about ternary operator

---
+++ Programming Fundamentals/Week 3/Ternay operator code/ternary_operator_code.c
@@ -0,0 +1,19 @@
+/* Ternary operator and flow of control
+by Isabel Lacerda
+Nov. 15, 2021 */
+
+#include <stdio.h>
+
+int main (){
+    int speed;
+    printf("Enter your speed as an integer: ");
+    scanf("%d", &speed);
+    speed = (speed < 0) ? (-1) : (speed <= 65) ? (65) : (speed <= 70) ? (70) : (90);
+    switch (speed) {
+        case 65: printf("\nNo speeding Ticket\n\n");break;
+        case 70: printf("\nSpeeding Ticket\n\n");break;
+        case 90: printf("\nExpensive Speeding Ticket\n\n");break;
+        case -1: printf("\nIncorrect speed\n\n");break;
+    }
+    return 0;
+}
